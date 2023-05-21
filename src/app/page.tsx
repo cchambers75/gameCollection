68bae2a329e76668d08911b6c18f69add409513a
Curtis Chambers <curtis.chambers@live.com>
2023-05-21 16:41:10 -0500

    removed excess component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,56 @@ import GameTable from "@/components/GameTable";
 import { Suspense } from "react";
 import { Press_Start_2P } from "next/font/google";
 import Loading from "./loading";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+type Games = {
+	title: string;
+	category: string;
+	playersMin: number;
+	playersMax: number;
+	playingTime: number;
+	link: string;
+};
 
 const pressStart2p = Press_Start_2P({ weight: "400", subsets: ["latin"] });
 
 export const revalidate = 0;
 
-export default function Home() {
+async function getGames() {
+	const doc = new GoogleSpreadsheet(
+		"15r7axbjFtiJqLKuU-zJpRIaE5x_NjlS7yQEtmBfMlbo"
+	);
+
+	doc.useApiKey(process.env.GOOGLE_API_KEY as string);
+
+	await doc.loadInfo();
+
+	const sheet = doc.sheetsByIndex[0];
+	const rows = await sheet.getRows();
+	const rowData: Games[] = rows.map((row) => {
+		return {
+			title: row["Title"],
+			category: row["Category"],
+			playersMin: parseInt(row["Players (Min)"]),
+			playersMax: parseInt(row["Players (Max)"]),
+			playingTime: parseInt(row["Playing Time (Min)"]),
+			link: row["Website Link"],
+		};
+	});
+	rowData.sort((a, b) => {
+		if (a.title < b.title) {
+			return -1;
+		} else if (a.title > b.title) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+	return rowData;
+}
+
+export default async function Home() {
+	const data = await getGames();
 	return (
 		<main className="flex min-h-screen flex-col items-center p-4 md:p-16">
 			<div className={pressStart2p.className}>
@@ -17,8 +61,7 @@ export default function Home() {
 			</div>
 
 			<Suspense fallback={<Loading />}>
-				{/* @ts-expect-error Server Component */}
-				<GameTable />
+				<GameTable data={data} />
 			</Suspense>
 		</main>
 	);
--- src/components/GameTable.tsx
+++ src/components/GameTable.tsx
@@ -1,5 +1,7 @@
-import { GoogleSpreadsheet } from "google-spreadsheet";
-import Table from "./Table";
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 type Games = {
 	title: string;
@@ -10,43 +12,160 @@ type Games = {
 	link: string;
 };
 
-async function getGames() {
-	const doc = new GoogleSpreadsheet(
-		"15r7axbjFtiJqLKuU-zJpRIaE5x_NjlS7yQEtmBfMlbo"
-	);
-
-	doc.useApiKey(process.env.GOOGLE_API_KEY as string);
-
-	await doc.loadInfo();
-
-	const sheet = doc.sheetsByIndex[0];
-	const rows = await sheet.getRows();
-	const rowData: Games[] = rows.map((row) => {
-		return {
-			title: row["Title"],
-			category: row["Category"],
-			playersMin: parseInt(row["Players (Min)"]),
-			playersMax: parseInt(row["Players (Max)"]),
-			playingTime: parseInt(row["Playing Time (Min)"]),
-			link: row["Website Link"],
-		};
-	});
-	rowData.sort((a, b) => {
-		if (a.title < b.title) {
-			return -1;
-		} else if (a.title > b.title) {
-			return 1;
-		} else {
-			return 0;
-		}
-	});
-	return rowData;
+function debounce(func: (...args: any[]) => void, wait: number) {
+	let timeout: NodeJS.Timeout;
+	return (...args: any[]) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => func(...args), wait);
+	};
 }
 
-const GameTable = async () => {
-	const data = await getGames();
+const GameTable: React.FC<{ data: Games[] }> = ({ data }) => {
+	const [searchTerm, setSearchTerm] = useState("");
+	const [sortConfig, setSortConfig] = useState<{
+		key: keyof Games;
+		direction: "ascending" | "descending";
+	} | null>(null);
+	const [searchResults, setSearchResults] = useState<Games[]>(data); // initialize with games list
 
-	return <Table data={data} />;
+	const handleSearch = debounce((value: string) => {
+		setSearchResults(
+			data.filter(
+				(game: Games) =>
+					game.title.toLowerCase().includes(value.toLowerCase()) ||
+					game.category.toLowerCase().includes(value.toLowerCase()) ||
+					(game.playersMin <= Number(value) &&
+						game.playersMax >= Number(value)) ||
+					game.playingTime === Number(value)
+			)
+		);
+	}, 500);
+
+	useEffect(() => {
+		handleSearch(searchTerm);
+	}, [searchTerm]);
+
+	useEffect(() => {
+		let sortedGames = [...searchResults];
+		if (sortConfig !== null) {
+			sortedGames.sort((a, b) => {
+				if (a[sortConfig.key] < b[sortConfig.key]) {
+					return sortConfig.direction === "ascending" ? -1 : 1;
+				}
+				if (a[sortConfig.key] > b[sortConfig.key]) {
+					return sortConfig.direction === "ascending" ? 1 : -1;
+				}
+				return 0;
+			});
+		}
+		setSearchResults(sortedGames);
+	}, [sortConfig]);
+
+	const requestSort = (key: keyof Games) => {
+		setSortConfig((prev) => {
+			if (prev && prev.key === key && prev.direction === "ascending") {
+				return { key, direction: "descending" };
+			}
+			return { key, direction: "ascending" };
+		});
+	};
+
+	return (
+		<div className="px-4 sm:px-6 lg:px-8 w-full">
+			<div className="sm:flex sm:justify-end">
+				<div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+					<input
+						type="text"
+						placeholder="Search"
+						value={searchTerm}
+						className="shadow-lg block w-full sm:text-md p-2 border-gray-300 rounded-md focus:border-blue-600 focus:ring-black"
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className="mt-8 flow-root">
+				<div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+					<div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+						<div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+							<table className="min-w-full divide-y divide-gray-300">
+								<thead className="bg-slate-200">
+									<tr className="divide-x divide-gray-200 rounded-md">
+										<th
+											scope="col"
+											className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-2"
+											onClick={() => requestSort("title")}
+										>
+											Title
+										</th>
+										<th
+											scope="col"
+											className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
+											onClick={() => requestSort("category")}
+										>
+											Category
+										</th>
+										<th
+											scope="col"
+											className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
+											onClick={() => requestSort("playersMin")}
+										>
+											Players
+										</th>
+										<th
+											scope="col"
+											className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900
+		                                sm:pr-0"
+											onClick={() => requestSort("playingTime")}
+										>
+											Playing Time
+										</th>
+										<th
+											scope="col"
+											className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-0"
+										>
+											More Info
+										</th>
+									</tr>
+								</thead>
+								<tbody className="divide-y divide-gray-200 bg-white">
+									{searchResults.map((game, idx) => (
+										<tr
+											key={idx}
+											className="divide-x divide-gray-200 even:bg-slate-100"
+										>
+											<td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-4">
+												{game.title}
+											</td>
+											<td className="whitespace-nowrap p-4 text-sm text-gray-500">
+												{game.category}
+											</td>
+											<td className="whitespace-nowrap p-4 text-sm text-gray-500">
+												{game.playersMin} to {game.playersMax}
+											</td>
+											<td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-0">
+												{game.playingTime === 0
+													? "Flexible"
+													: `${game.playingTime} min`}
+											</td>
+											<td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-0">
+												{game.link ? (
+													<Link target="_blank" href={game.link}>
+														<FaExternalLinkAlt />
+													</Link>
+												) : (
+													""
+												)}
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default GameTable;
