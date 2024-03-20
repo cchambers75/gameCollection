b12be252f0c7cb8326368b8edb98549a76eebead
Curtis Chambers <curtis.chambers@live.com>
2024-03-19 22:51:47 -0500

    changed image import for loading page.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
+import whiteChessPiece from "../../public/whiteChessPiece.png";
 
 export default function Loading() {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
 			<Image
-				src={"/whiteChessPiece.png"}
+				src={whiteChessPiece}
 				alt="Queen Chess Piece"
 				width={300}
 				height={300}
 				className="animate-spin"
+				priority
 			/>
 		</div>
 	);
